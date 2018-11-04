a5de74543ddaf26afdf44609d06602becd769055
masaya-ishioka <penpenmono31@gmail.com>
2018-11-04 06:41:56 +0000

    [Finish]6-4

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '4989e337de99dda93880ab918ffac98945acb5d4280008732eb6a98b312d6cdfc6eb3cb22e97c9a2cba241a25c3949dc451458454b42b2378695b7a4ab9eebc2'
